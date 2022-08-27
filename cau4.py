ec28b5a7a1c8d24327da20290a293793c41c1905
phucluu <phuc.luu@localhost>
2022-08-27 21:06:41 +0700

    upload

---
+++ cau4.py
@@ -0,0 +1,37 @@
+from urllib import response
+import requests
+import json
+
+baseUrl = "https://api.meraki.com/api/v1"
+API_key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+org_id = "681155"
+
+def get_org_inv():
+    url = baseUrl + "/organizations/{}/inventory/devices".format(org_id)
+
+    header = {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json",
+        "X-Cisco-Meraki-API-Key" : API_key
+    }
+
+    response = requests.get(url, headers=header)
+
+    data_inv = response.json()
+
+    parse_data_inv = json.dumps(data_inv, indent=4)
+
+    print("Cau b")
+    print(parse_data_inv)
+    print("--------------------------------------------")
+
+    print("Cau c")
+    deviceid_null = []
+    for i in data_inv:
+        if i["networkId"] == None:
+            deviceid_null.append(i)
+    print(json.dumps(deviceid_null, indent=4))
+    print("--------------------------------------------")
+
+if __name__ == "__main__":
+    get_org_inv()
